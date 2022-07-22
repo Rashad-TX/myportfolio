aaff81c25371105344e319ae594d09648cc4585f
Rashad Moore <rjm4god7@gmail.com>
2022-07-21 22:36:24 -0500

    reverted link back for netlify

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
   return (
     <div className="footer">
         <div> 
-           <a href="mailto:rashad@rashadmoore.dev" target="_blank" rel="noreferrer"><img src={Email} className="bio-icon email" alt="icon"/></a><a href="https://github.com/Rashad-TX?tab=repositories" target="_blank"><img src={Github} alt="icon" className="bio-icon github"/> </a>
+           <a href="mailto:rashad@rashadmoore.dev" target="_blank" rel="noopener noreferrer"><img src={Email} className="bio-icon email" alt="icon"/></a><a href="https://github.com/Rashad-TX?tab=repositories" target="_blank"><img src={Github} alt="icon" className="bio-icon github"/> </a>
         </div>
             <p className="footer-text">&copy; Rashad Moore {new Date().getFullYear()}</p>
     </div>

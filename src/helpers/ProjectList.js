ca682086de9a8de921d312cfb90c5e5dcf89592f
Rashad Moore <rjm4god7@gmail.com>
2022-08-14 12:07:43 -0500

    removed sprouts

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,5 @@
 import Proj1 from "../assets/warriors1.jpg";
 import Proj2 from "../assets/mods1.jpg";
-import Proj3 from "../assets/sprouts1.jpg";
 import Proj4 from "../assets/memgame2.jpg";
 import Proj5 from "../assets/timeflies1.jpg";
 import Proj6 from "../assets/quiz.png";
@@ -20,13 +19,6 @@ export const ProjectList = [
     skills: "Bootstrap,JavaScript,HTML,CSS",
     website: "https://modderz.netlify.app/",
     repository: "https://github.com/Rashad-TX/modderz"
-  },
-  {
-    name: "Website w/API",
-    image: Proj3,
-    skills: "JavaScript,API,HTML,CSS",
-    website: "https://sproutsbar.netlify.app/",
-    repository: "https://github.com/Rashad-TX/sprouts_clone"
   },
   {
     name: "Vanilla JavaScript Game",
